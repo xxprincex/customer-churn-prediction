b8254f3021de2184863ac44e3e2635c4ef4a6a98
prince <ps5597010@gmail.com>
2025-04-25 20:21:36 +0530

    CUSTOMER CHURN PREDICTION API

--- app.py
+++ app.py
@@ -553,4 +553,5 @@ def home():
     return "Customer Churn Prediction API is running!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
